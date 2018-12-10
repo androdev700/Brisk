f896c2bc646b0506cdafd18865a08414e9f881fb
Ayush Singh <androdev700@gmail.com>
2018-12-11 01:06:03 +0530

    Initial Commit

---
+++ AndroBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AndroBar
+//
+//  Created by Ayush Singh on 11/12/18.
+//  Copyright © 2018 Ayush Singh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AndroBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AndroBar
+//
+//  Created by Ayush Singh on 11/12/18.
+//  Copyright © 2018 Ayush Singh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
