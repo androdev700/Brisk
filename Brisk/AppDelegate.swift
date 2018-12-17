4c2be970a7a9db6a2d4e58c6a10f5ad9088e89f9
Ayush Singh <androdev700@gmail.com>
2018-12-17 09:42:43 +0530

    Bug Fix

--- Brisk/AppDelegate.swift
+++ Brisk/AppDelegate.swift
@@ -18,5 +18,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        return true
+    }
 }
 
